142eaa0468cefb72eb10a24e8ac56ea50e428eef
Jake Mareno <jmareno@unc.edu>
2024-03-25 16:53:36 -0400

    main canvas rendering

--- behavior.js
+++ behavior.js
@@ -1,7 +1,7 @@
-import { Vpl } from './data.js';
-import { Vpp } from './data.js';
-import { Fpl } from './data.js';
-import { Fpp } from './data.js';
+//import { Vpl } from './data.js';
+//import { Vpp } from './data.js';
+//import { Fpl } from './data.js';
+//import { Fpp } from './data.js';
 console.clear();
 
 // ----------------------------------------------
@@ -86,24 +86,27 @@ let canvas_main = null;
 let canvas_xz = null;
 let canvas_yz = null;
 let canvas_xy = null;
-let canvases = [canvas_main, canvas_xz, canvas_yz, canvas_xy]
+let canvases = null;
 
 // WebGL Contexts
 let gl_main = null;
 let gl_xz = null;
 let gl_yz = null;
 let gl_xy = null;
-let contexts = [gl_main, gl_xz, gl_yz, gl_xy]
+let contexts = null;
 
 // Programs
 let program_main = null;
 let program_xz = null;
 let program_yz = null;
 let program_xy = null;
-let programs = [program_main, program_xz, program_yz, program_xy]
+let programs = null;
 
 let attr_vertex = null;
 let vertex_data = [];
+let size = 3;
+let axis_index = 0;
+let prop_offset = -0.375;
 
 function createVertexData() {
     
@@ -116,6 +119,7 @@ function createVertexData() {
         vertex_data[row++] = Vpl[ Fpl[i][2] ];
     }
 
+    // add propeller vertices + faces to vertex.data
     for ( let i=0; i<Fpp.length; i++ ) {
         vertex_data[row++] = Vpp[ Fpp[i][0] ];
         vertex_data[row++] = Vpp[ Fpp[i][1] ];
@@ -136,31 +140,42 @@ function configure() {
     canvas_xz = document.getElementById("xz");
     canvas_yz = document.getElementById("yz");
     canvas_xy = document.getElementById("xy");
+    canvases = [canvas_main, canvas_xz, canvas_yz, canvas_xy];
 
     gl_main = canvas_main.getContext("webgl");
     gl_xz = canvas_xz.getContext("webgl");
     gl_yz = canvas_yz.getContext("webgl");
     gl_xy = canvas_xy.getContext("webgl");
+    contexts = [gl_main, gl_xz, gl_yz, gl_xy];
 
     let program_main = initShaders(gl_main, "vertex-shader", "fragment-shader");
     let program_xz = initShaders(gl_xz, "vertex-shader", "fragment-shader");
     let program_yz = initShaders(gl_yz, "vertex-shader", "fragment-shader");
     let program_xy = initShaders(gl_xy, "vertex-shader", "fragment-shader");
+    programs = [program_main, program_xz, program_yz, program_xy];
 
     gl_main.useProgram(program_main);
     gl_xz.useProgram(program_xz);
     gl_yz.useProgram(program_yz);
     gl_xy.useProgram(program_xy);
 
-    gl_main.viewport( 0, 0, canvas.width, canvas.height );
-    gl_main.enable( webgl_context.DEPTH_TEST );
+    gl_main.viewport( 0, 0, canvas_main.width, canvas_main.height );
+    gl_main.enable( gl_main.DEPTH_TEST );
 
-    attr_vertex = gl_main.getAttribLocation(program_main, "vertex");
-    uniform_props = gl_main.getUniformLocation(program_main, "props");
-    uniform_color = gl_main.getUniformLocation( program, "color" );
-    uniform_z_translation = gl_main.getUniformLocation(program, "z_translation");
+    /* for(let i = 0; i < 4; i++) {
+        console.log(contexts);
+        let gl = contexts[i];
+        let program = programs[i];
+        attr_vertex = gl.getAttribLocation(program, "vertex");
+        uniform_props = gl.getUniformLocation(program, "props");
+        uniform_color = gl.getUniformLocation( program, "color" );
+        uniform_z_translation = gl.getUniformLocation(program, "z_translation");
+    } */
 
-
+    attr_vertex = gl_main.getAttribLocation( program_main, "vertex" );
+    uniform_props = gl_main.getUniformLocation( program_main, "props" );
+    uniform_color = gl_main.getUniformLocation( program_main, "color" );
+    uniform_z_translation = gl_main.getUniformLocation(program_main, "z_translation");
 }
 
 function allocateMemory() {
@@ -175,7 +190,35 @@ function allocateMemory() {
 }
 
 function draw() {
-    
+    let gl = gl_main;
+
+    console.log([xang, yang, zang]);
+    gl.uniform4f(uniform_props,
+                xang * Math.PI/180,
+                yang * Math.PI/180,
+                zang * Math.PI/180,
+                1.75);
+
+    //gl.uniform4f( uniform_color, 0.60, 0.60, 0.60, 1.0 );
+    gl.uniform4f( uniform_color, 0.3,0.3,0.3,1);
+    for( let j = 0; j < Fpl.length*3; j += 3 ) {
+        gl.drawArrays(gl.LINE_STRIP, j, size);
+    }
+
+    gl.uniform1f( uniform_z_translation, prop_offset);
+    for( let j = Fpl.length*3; j < axis_index; j += 3 ) {
+        gl.drawArrays(gl.LINE_STRIP, j, size);
+    }
+    gl.uniform1f( uniform_z_translation, 0);
+
+    gl.uniform4f( uniform_color, 0.81, 0.81, 0.81, 1.0 ); 
+    gl.drawArrays(gl.TRIANGLES, 0, Fpl.length*3);
+
+    gl.uniform1f( uniform_z_translation, prop_offset);
+    gl.drawArrays(gl.TRIANGLES, Fpl.length*3+1, Fpp.length);
+    gl.uniform1f( uniform_z_translation, 0);
+
+    rot = (rot + rot_inc) % 360;
 }
 
 
@@ -186,4 +229,5 @@ function draw() {
 createVertexData();
 configure();
 allocateMemory();
+//draw();
 setInterval(draw, 100);
